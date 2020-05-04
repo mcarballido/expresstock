38c2b28d56ce9ea2c5afdb205b9998ca58535e4e
Manuel Carballido <manu.carba96@gmail.com>
2020-05-04 18:15:54 -0300

    First commit

---
+++ backend/src/test/java/com/mcarballido/expresstock/ExpresstockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mcarballido.expresstock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpresstockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
